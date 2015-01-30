ed793b894448a8c76331b4fdfa8d56e8089f2745
Stav <stav@ftbpro.com>
2015-01-30 20:57:17 +0200

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UpdatesImporterRepo::Application.config.secret_token = 'fb081da57b83a0cbb39cbdcae6ed0e19acaf90d305c549acca832c986d0d2fb3d567c4dbbe95cae8c880a68a5751f524e7eccf3b44c8e4a819e4c885b6d3ded0'
